27f4b22aee0f8d16fde849f399b34717279da4d0
CamilleAbella <camille.abella@hotmail.fr>
2021-02-24 17:35:59 +0100

    add hub management commands

--- src/commands/hubs.ts
+++ src/commands/hubs.ts
@@ -2,11 +2,64 @@ import * as app from "../app"
 
 const command: app.Command = {
   name: "hubs",
-
+  aliases: ["hub", "net"],
+  networkOwner: true,
   async run(message) {
-    // todo: code here
-    await message.reply("hubs command is not yet implemented.")
+    const network = app.networks.get(message.author.id) as app.Network
+    return message.channel.send(
+      new app.MessageEmbed()
+        .setTitle(`Hub list - ${network.displayName}`)
+        .setDescription(
+          app
+            .getNetworkHubs(message.author.id)
+            .map((hub, id) => {
+              return `\`${id}\` ${
+                hub.inviteLink
+                  ? `[invitation link](${hub.inviteLink})`
+                  : "no invitation link"
+              } **${
+                (message.client.channels.cache.get(id) as app.GuildChannel)
+                  ?.guild.name ?? "not a guild channel"
+              }**`
+            })
+            .join("\n")
+        )
+    )
   },
+  subs: [
+    {
+      name: "remove",
+      aliases: ["rm"],
+      positional: [
+        {
+          name: "hubId",
+          required: true,
+        },
+      ],
+      networkOwner: true,
+      async run(message) {
+        const { hubId } = message.positional
+
+        const hub = app.hubs.get(hubId)
+        const network = app.networks.get(message.author.id) as app.Network
+
+        if (!hub) return message.channel.send("The given hub id is incorrect.")
+        if (hub.networkId !== message.author.id)
+          return message.channel.send(
+            "The targeted hub does not belong to you."
+          )
+
+        app.removeHub.bind(message.client)(
+          hubId,
+          `This hub was removed by the "**${network.displayName}**" owner.`
+        )
+
+        return message.channel.send(
+          "The targeted hub was successfully removed."
+        )
+      },
+    },
+  ],
 }
 
 module.exports = command
--- src/commands/teardown.ts
+++ src/commands/teardown.ts
@@ -5,12 +5,12 @@ const command: app.Command = {
   networkOwner: true,
   aliases: ["down", "abort", "clean", "delete", "remove", "reset"],
   async run(message) {
+    const network = app.networks.get(message.author.id) as app.Network
+
     app.removeNetwork.bind(message.client)(message.author.id)
 
     return message.channel.send(
-      `You have successfully removed the "**${
-        app.networks.get(message.author.id)?.displayName
-      }**" network.`
+      `You have successfully removed the "**${network.displayName}**" network.`
     )
   },
 }
