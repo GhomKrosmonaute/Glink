02eb7d1b2697462ef0872a766b5e834854ce10fc
CamilleAbella <camille.abella@hotmail.fr>
2021-03-30 11:06:57 +0200

    edit networks command

--- src/commands/networks.ts
+++ src/commands/networks.ts
@@ -2,47 +2,65 @@ import * as app from "../app"
 
 const command: app.Command = {
   name: "networks",
+  description: "Networks manager",
   aliases: ["nw", "works"],
   async run(message) {
-    new app.Paginator(
-      app.Paginator.divider([...app.networks.entries()], 10).map((page) => {
-        return new app.MessageEmbed().setTitle("Networks list").setDescription(
-          page
-            .map(([networkId, network]) => {
-              const networkUsers = new Set(
-                app
-                  .getNetworkHubs(networkId)
-                  .map((hub, hubId) => {
-                    const channel = message.client.channels.cache.get(
-                      hubId
-                    ) as app.GuildChannel
-                    return channel.guild.members.cache
-                      .filter(({ user }) => !user.bot)
-                      .map((member) => member.id)
-                  })
-                  .flat()
-              )
-
-              return `\`${app.resizeText(
-                network.displayName,
-                20,
-                true
-              )}\` - [ ${
-                networkUsers.size
-              } 👤 ] - owner: ${message.client.users.cache.get(networkId)}`
-            })
-            .join("\n")
-        )
-      }),
-      message.channel,
-      (reaction, user) => user.id === message.author.id
+    return message.channel.send(
+      "Type `" +
+        (await app.prefix(message.guild)) +
+        "help networks` for usage detail."
     )
   },
   subs: [
     {
+      name: "list",
+      description: "List networks",
+      async run(message) {
+        new app.Paginator(
+          app.Paginator.divider([...app.networks.entries()], 10).map((page) => {
+            return new app.MessageEmbed()
+              .setTitle("Networks list")
+              .setDescription(
+                page
+                  .map(([networkId, network]) => {
+                    const networkUsers = new Set(
+                      app
+                        .getNetworkHubs(networkId)
+                        .map((hub, hubId) => {
+                          const channel = message.client.channels.cache.get(
+                            hubId
+                          ) as app.GuildChannel
+                          return channel.guild.members.cache
+                            .filter(({ user }) => !user.bot)
+                            .map((member) => member.id)
+                        })
+                        .flat()
+                    )
+
+                    return `\`${app.resizeText(
+                      network.displayName,
+                      20,
+                      true
+                    )}\` - [ ${
+                      networkUsers.size
+                    } 👤 ] - owner: ${message.client.users.cache.get(
+                      networkId
+                    )}`
+                  })
+                  .join("\n")
+              )
+          }),
+          message.channel,
+          (reaction, user) => user.id === message.author.id
+        )
+      },
+    },
+    {
       name: "remove",
       botOwner: true,
-      async run(message) {},
+      async run(message) {
+        return message.channel.send("not yet implemented.")
+      },
     },
   ],
 }
